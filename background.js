c89d2f6066b41c1c95c1f427adb263731ad3cda8
Arun Sundaram <a.sundaram@chaostheorygames.com>
2021-04-11 23:15:48 +1000

    Made constants for default option values

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const Default_Threshhold 				= 90;
+const Default_Hide_Watched 				= true;
+const Default_Hide_AutoPlaylist 		= true;
+const Default_Hide_UserPlaylists 		= false;
+const Default_EnablePage_Video 			= true;
+const Default_EnablePage_Home 			= true;
+const Default_EnablePage_Subscriptions 	= true;
+const Default_EnablePage_Channel 		= false;
+const Default_EnablePage_Search 		= false;
+
+
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
@@ -14,7 +25,17 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 function initStorage(){
-	return {bMix:true, bWatched:true, bPlaylist:false, bSubs:true, threshhold:100, bSearch:false, bChannel:false, bHome:true, bNext:true};
+	return {
+		bMix: 		Default_Hide_AutoPlaylist,
+		bWatched: 	Default_Hide_Watched,
+		bPlaylist: 	Default_Hide_UserPlaylists,
+		bSubs: 		Default_EnablePage_Subscriptions,
+		threshhold: Default_Threshhold,
+		bSearch: 	Default_EnablePage_Search,
+		bChannel: 	Default_EnablePage_Channel,
+		bHome: 		Default_EnablePage_Home,
+		bNext: 		Default_EnablePage_Video
+	};
 }
 
 function storageSet(key, val){
